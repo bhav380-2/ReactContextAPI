424a5967e308d313d1c5c9c0282a7a352d56f1db
bhav380 <bhaweshrathour18@gmail.com>
2024-02-22 18:43:38 +0530

    learning React context-api, useContext hook, producer and consumer component, custom context Provider

---
+++ src/itemContext.js
@@ -0,0 +1,24 @@
+import {createContext} from "react";
+import { useState } from "react";
+
+const itemContext = createContext();
+
+function CustomItemContext({children}){
+
+    const [total, setTotal] = useState(0);
+    const [item, setItem] = useState(0);
+
+
+
+    return (
+        <itemContext.Provider value={{total,setTotal,item,setItem}}>
+            {children}
+
+        </itemContext.Provider>
+    )
+
+}
+
+export {itemContext};
+
+export default CustomItemContext;
---
+++ src/totalContext.js
@@ -0,0 +1,5 @@
+import {createContext, createElement} from "react";
+
+const totalContext = createContext();
+
+export {totalContext};
